b4ad7ddfea19fcbc776ae3bc1ff0b3e5c96d92ab
RAISY <24je0638@iitism.ac.in>
2024-10-24 10:55:50 +0530

    Inverse implemented

--- calculator.c
+++ calculator.c
@@ -74,7 +74,7 @@ int main() {
             case 'i':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("The result of %d is %f\n",a,inverse of a);
                 break;
             default:
                 printf("Invalid operation. Please try again.\n");
